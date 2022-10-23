12c8c013630279a15fee9028da0ae843c96e498d
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2022-10-23 13:30:27 +0200

    added components and apis

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
-const PORT = 5000;
+// a sample usage shown. Change it and add your own
+app.get('/customdata', (req, res) => {
+    const customData = [
+        {
+            id: 1,
+            firstName: 'Ted',
+            lastName: 'Mosby'
+        },
+        {
+            id: 2,
+            firstName: 'Barney',
+            lastName: 'Stinson'
+        },
+        {
+            id: 3,
+            firstName: 'Robin',
+            lastName: 'Scherbatsky'
+        },
+        {
+            id: 4,
+            firstName: 'Marshall',
+            lastName: 'Eriksen'
+        },
+        {
+            id: 5,
+            firstName: 'Lily',
+            lastName: 'Aldrin'
+        }
+    ]
 
+    res.json(customData)
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => `Server is running on port ${PORT}`)
